384f6d999e2b1b820839bec271edc1aabbd1e965
yl12510 <yu.liu8904@gmail.com>
2022-06-10 16:08:29 +0000

    add pipeline

--- src/getting_started/getting_started.py
+++ src/getting_started/getting_started.py
@@ -70,3 +70,27 @@ StandardScaler().fit(X).transform(X)
 # -
 
 # ## Pipelines: chaining pre-processors and estimators
+# - a pipeline offers the same API functions e.g. `fit` and `predict` as a regular estimator
+# - using a pipeline can prevent from disclosing testing data in training data (i.e. data leakage)
+
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+
+# +
+from sklearn.preprocessing import StandardScaler
+
+# create a pipeline object
+pipe = make_pipeline(StandardScaler(), LogisticRegression())
+
+# load the iris dataset and split into train and test sets
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# fit the whole pipeline
+pipe.fit(X_train, y_train)
+
+# use it to predict over test data set and calculate accuracy score
+accuracy_score(pipe.predict(X_test), y_test)
